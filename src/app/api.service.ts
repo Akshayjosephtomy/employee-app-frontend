9e8ed6cb3ad1ce839a10a9ff88648f2a26b370d7
Akshayjosephtomy <akshayjt007@gmail.com>
2022-10-21 01:36:03 +0530

    e

---
+++ src/app/api.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+
+  viewEmployee=()=>{
+    return this.http.get("http://localhost:8080/viewemployee")
+  }
+
+  addEmployee=(data:any)=>{
+    return this.http.post("http://localhost:8080/addemployee",data)
+  }
+
+}
